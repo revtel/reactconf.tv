40ea4b344c10dbc4eab50d19ebfc0a19661d5115
Richie <whitedogg13@gmail.com>
2020-12-17 20:58:02 +0800

    fix: change site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,8 +25,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `ReactConf.tv`,
+        short_name: `ReactConf.tv`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
