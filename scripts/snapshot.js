d8f2ad9af665ac6e5d77f2aaddb3b46aef35a7ba
Richie <whitedogg13@gmail.com>
2021-02-02 21:32:46 +0800

    feat: fetch view count and save to snapshot

--- scripts/snapshot.js
+++ scripts/snapshot.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
+const fetch = require('node-fetch');
 const log = console.log;
+const playlistPath = 'static/playlistitems';
+const snapshotPath = 'static/snapshots';
 
 function readJsonFile(filepath) {
   try {
@@ -12,23 +15,42 @@ function readJsonFile(filepath) {
   }
 }
 
+async function fetchViewStats(videoIdList) {
+  const apiKey = 'AIzaSyBJLChpvbusFt4k8b4SHRShSHTrZNUA9Q8';
+  const statsResp = await fetch(
+    `https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id=${videoIdList.join(
+      ',',
+    )}&key=${apiKey}&maxResults=100`,
+    {
+      header: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    },
+  );
+
+  return statsResp.json();
+}
+
 async function snapshot() {
   log(chalk.gray('scanning static/playlistitems') + `\n`);
 
-  const playlistPath = 'static/playlistitems';
   const playlists = fs.readdirSync(playlistPath);
   const snapshotJson = {};
 
   log('total ' + chalk.gray(`total ${playlists.length}`) + ' playlists');
   for (const fname of playlists) {
+    log('processing ' + chalk.gray(fname));
+
     const filepath = path.join(playlistPath, fname);
     const {items} = readJsonFile(filepath);
+    const videoIdList = [];
+
     for (let idx = 0; idx < items.length; idx++) {
       const video = items[idx];
       const {title, playlistId, publishedAt, thumbnails} = video.snippet;
       const {videoId} = video.contentDetails;
       const thumbnail = (thumbnails.high && thumbnails.high.url) || null;
-      // TODO: need watch count
       snapshotJson[videoId] = {
         playlistId,
         idx,
@@ -37,15 +59,22 @@ async function snapshot() {
         thumbnail,
         videoId,
       };
+      videoIdList.push(videoId);
+    }
+
+    const statsRespJson = await fetchViewStats(videoIdList);
+
+    for (const item of statsRespJson.items) {
+      snapshotJson[item.id].stats = item.statistics;
     }
   }
-
-  const snapshotPath = 'static/snapshots';
 
   fs.writeFileSync(
     path.join(snapshotPath, `${new Date().getTime()}.json`),
     JSON.stringify(snapshotJson, null, 2),
   );
+
+  log(chalk.green('Done'));
   return true;
 }
 
