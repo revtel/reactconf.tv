b123063efc0c337f8763086eb100a46e26d5741d
guychienll <guychienll@gmail.com>
2021-04-26 22:57:17 +0800

    feat: video version for top10 and new releases

--- src/templates/LandingPage.js
+++ src/templates/LandingPage.js
@@ -11,8 +11,8 @@ import VideoItemList from '../components/VideoItemList';
 import TalkListPanel from '../components/TalkListPanel';
 import SelectChannelBtn from '../components/SelectChannelBtn';
 import groupConfByYear from '../utils/groupConfByYear';
-import getTop10Seminars from '../utils/getTrendingNow';
-import getNewReleases from '../utils/getNewReleases';
+import getTop10Seminars, {getTop10Videos} from '../utils/getTrendingNow';
+import getNewReleases from '../utils/getNewReleaseVideos';
 import getMostViewed from '../utils/getMostViewed';
 
 function LandingPage(props) {
@@ -50,11 +50,9 @@ function LandingPage(props) {
 
   const classicVideos = getMostViewed();
 
-  const top10Seminars = useMemo(() => getTop10Seminars(channels), [channels]);
+  const top10Videos = getTop10Videos();
 
-  const newReleasesSeminars = useMemo(() => getNewReleases(channels), [
-    channels,
-  ]);
+  const newReleaseVideos = getNewReleases();
 
   return (
     <>
@@ -84,44 +82,8 @@ function LandingPage(props) {
             </div>
           )}
 
-          {newReleasesSeminars.length > 0 && !selectedChannel && (
-            <div className="new-releases">
-              <Widgets.FlexRow>
-                <Label style={{marginLeft: 30, marginRight: 10}}>
-                  New Releases
-                </Label>
-                <Widgets.Badge style={{marginLeft: 8}}>
-                  {newReleasesSeminars.length}
-                </Widgets.Badge>
-              </Widgets.FlexRow>
-
-              <SeminarItemList
-                items={newReleasesSeminars}
-                onItemClick={(seminar) => bottomPanelRef.current.open(seminar)}
-              />
-            </div>
-          )}
-
-          {top10Seminars.length > 0 && !selectedChannel && (
-            <div className="trending-now">
-              <Widgets.FlexRow>
-                <Label style={{marginLeft: 30, marginRight: 10}}>
-                  Trending Now
-                </Label>
-                <Widgets.Badge style={{marginLeft: 8}}>
-                  {top10Seminars.length}
-                </Widgets.Badge>
-              </Widgets.FlexRow>
-
-              <SeminarItemList
-                items={top10Seminars}
-                onItemClick={(seminar) => bottomPanelRef.current.open(seminar)}
-              />
-            </div>
-          )}
-
           {classicVideos.length > 0 && !selectedChannel && (
-            <div className="trending-now">
+            <div className="classic">
               <Widgets.FlexRow>
                 <Label style={{marginLeft: 30, marginRight: 10}}>Classic</Label>
                 <Widgets.Badge style={{marginLeft: 8}}>
@@ -130,6 +92,36 @@ function LandingPage(props) {
               </Widgets.FlexRow>
 
               <VideoItemList items={classicVideos} />
+            </div>
+          )}
+
+          {top10Videos.length > 0 && !selectedChannel && (
+            <div className="trending-now">
+              <Widgets.FlexRow>
+                <Label style={{marginLeft: 30, marginRight: 10}}>
+                  Trending Now
+                </Label>
+                <Widgets.Badge style={{marginLeft: 8}}>
+                  Most Populars Videos
+                </Widgets.Badge>
+              </Widgets.FlexRow>
+
+              <VideoItemList items={top10Videos} />
+            </div>
+          )}
+
+          {newReleaseVideos.length > 0 && !selectedChannel && (
+            <div className="new-release">
+              <Widgets.FlexRow>
+                <Label style={{marginLeft: 30, marginRight: 10}}>
+                  New Releases
+                </Label>
+                <Widgets.Badge style={{marginLeft: 8}}>
+                  New Releases Videos
+                </Widgets.Badge>
+              </Widgets.FlexRow>
+
+              <VideoItemList items={newReleaseVideos} />
             </div>
           )}
 
