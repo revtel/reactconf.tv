d16dbc89b017b598885f6979a0c0b26a36433415
Richie <whitedogg13@gmail.com>
2021-04-28 08:16:59 +0800

    fix: minor issue for getMostViewed

--- src/utils/getMostViewed.js
+++ src/utils/getMostViewed.js
@@ -2,8 +2,9 @@ import latest from '../../static/snapshots/latest.json';
 
 const getMostViewed = () => {
   return Object.values(latest)
-    .filter((item) => item.stats)
-    .sort((a, b) => b.stats.viewCount - a.stats.viewCount);
+    .filter((item) => item.stats?.viewCount)
+    .sort((a, b) => parseInt(b.stats.viewCount) - parseInt(a.stats.viewCount))
+    .slice(0, 10);
 };
 
 export default getMostViewed;
